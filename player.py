8b01b413767bdbe0902c70f448a986c184e284f2
Wilt Moise <wiltmoise@gmail.com>
2022-05-20 23:09:09 -0400

    testing stuff out on a diff branch

--- player.py
+++ player.py
@@ -1,15 +1,28 @@
 class player:
 
     has_pos = False;
+    speed = 12;
 
-    def init(self):
+    # this will be the current entity to manipulate
+    currently_controlled = None;
+
+
+    def init(self, entity):
+
+        self.currently_controlled = entity
         # entity
         pass
 
     
+
+    # need to figure out what determines them gaining possesion
+    def gained_possesion(self):
+
+        # need a check to see if its a cpu that got the ball and not the player
+        self.speed = 10
+
     def move_around(self):
         pass
-
 
     def boost(self):
         pass

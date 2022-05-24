43af4d6bbdfd7a6bc9ad31e05419bb430ddca75d
Wilt Moise <wiltmoise@gmail.com>
2022-05-24 11:05:20 -0400

    added class that will be inherited from

---
+++ character.py
@@ -0,0 +1,16 @@
+
+
+# both teammate and opp will inherit from this class
+
+class character:
+
+    # animations
+
+    def __init__(self, position):
+        print("ok")
+
+    def change_direction(self):
+
+        # not only should this change the idle facing direction, but it should
+        # load the running animation for said direction
+        pass
